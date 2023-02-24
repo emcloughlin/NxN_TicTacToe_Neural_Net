305f28e4cef52a0f0eca4fac8c6d2b1dccaf7889
Evan McLoughlin <emcloughlin42@gmail.com>
2023-02-23 20:55:27 -0500

    Updated requirements

--- neural.py
+++ neural.py
@@ -10,24 +10,31 @@ https://iamtrask.github.io/2015/07/12/basic-python-network/
 
 from IPython import get_ipython
 get_ipython().magic('reset -sf')
+# %%
 import numpy as np
 import matplotlib.pyplot as pyplot
 import pandas as pd
+# %%
 
+# %%
 # sigmoid function
 def nonlin(x,deriv=False):
     if(deriv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
-    
+
+# %%
+
 # input dataset  where each row is a training example
 X = np.array([  [0,0,1],
                 [0,1,1],
                 [1,0,1],
                 [1,1,1] ])
-    
-# output dataset    where each row is a training example         
+
+# output dataset    where each row is a training example
 y = np.array([[0,0,1,1]]).T
+
+# %%
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
@@ -35,6 +42,8 @@ np.random.seed(1)
 
 # initialize weights randomly with mean 0
 syn0 = 2*np.random.random((3,1)) - 1
+# %%
+
 #Singlee layer
 for iter in np.arange(10000):
 
@@ -45,7 +54,7 @@ for iter in np.arange(10000):
     # how much did we miss?
     l1_error = y - l1
 
-    # multiply how much we missed by the 
+    # multiply how much we missed by the
     # slope of the sigmoid at the values in l1
     l1_delta = l1_error * nonlin(l1,True)
     # update weights
@@ -53,6 +62,8 @@ for iter in np.arange(10000):
 
 print("Output After Training:")
 print(l1)
+
+# %%
 
 
 #####Two layers
@@ -62,12 +73,14 @@ def nonlin(x,deriv=False):
 	    return x*(1-x)
 
 	return 1/(1+np.exp(-x))
-    
+
+# %%
+
 X = np.array([[0,0,1],
             [0,1,1],
             [1,0,1],
             [1,1,1]])
-                
+
 y = np.array([[0],
 			[1],
 			[1],
@@ -79,6 +92,8 @@ np.random.seed(1)
 syn0 = 2*np.random.random((3,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
 
+# %%
+
 for j in np.arange(60000):
 
 	# Feed forward through layers 0, 1, and 2
@@ -88,23 +103,25 @@ for j in np.arange(60000):
 
     # how much did we miss the target value?
     l2_error = y - l2
-    
+
     if (j% 10000) == 0:
         print("Error:" + str(np.mean(np.abs(l2_error))))
-        
+
     # in what direction is the target value?
     # were we really sure? if so, don't change too much.
     l2_delta = l2_error*nonlin(l2,deriv=True)
 
     # how much did each l1 value contribute to the l2 error (according to the weights)?
     l1_error = l2_delta.dot(syn1.T)
-    
+
     # in what direction is the target l1?
     # were we really sure? if so, don't change too much.
     l1_delta = l1_error * nonlin(l1,deriv=True)
 
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
+
+# %%
 
 ####USING SKLearn
 #https://stackabuse.com/introduction-to-neural-networks-with-scikit-learn/
@@ -126,13 +143,13 @@ X = irisdata.iloc[:, 0:4]
 y = irisdata.select_dtypes(include=[object])
 y.head()
 #You can see that the values in the y series are categorical.
-#However, neural networks work better with numerical data. 
+#However, neural networks work better with numerical data.
 #Our next task is to convert these categorical values to numerical values.
-# But first let's see how many unique values we have in our y series. 
+# But first let's see how many unique values we have in our y series.
 y.Class.unique()
 
-#We have three unique classes 'Iris-setosa', 'Iris-versicolor' and 'Iris-virginica'. 
-#Let's convert these categorical values to numerical values. 
+#We have three unique classes 'Iris-setosa', 'Iris-versicolor' and 'Iris-virginica'.
+#Let's convert these categorical values to numerical values.
 #To do so we will use Scikit-Learn's LabelEncoder class.
 
 from sklearn import preprocessing
