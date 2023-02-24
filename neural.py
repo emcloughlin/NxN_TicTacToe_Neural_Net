63985819fe1be47d9fda47b1e466d44162c8fc53
Evan McLoughlin <emcloughlin42@gmail.com>
2023-02-23 20:15:43 -0500

    Added Cogan's neural net python file

---
+++ neural.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 11:40:26 2022
+Neural net simple
+https://iamtrask.github.io/2015/07/12/basic-python-network/
+@author: cogan
+"""
+
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+import numpy as np
+import matplotlib.pyplot as pyplot
+import pandas as pd
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+    
+# input dataset  where each row is a training example
+X = np.array([  [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1] ])
+    
+# output dataset    where each row is a training example         
+y = np.array([[0,0,1,1]]).T
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+#Singlee layer
+for iter in np.arange(10000):
+
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print("Output After Training:")
+print(l1)
+
+
+#####Two layers
+
+def nonlin(x,deriv=False):
+	if(deriv==True):
+	    return x*(1-x)
+
+	return 1/(1+np.exp(-x))
+    
+X = np.array([[0,0,1],
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]])
+                
+y = np.array([[0],
+			[1],
+			[1],
+			[0]])
+
+np.random.seed(1)
+
+# randomly initialize our weights with mean 0
+syn0 = 2*np.random.random((3,4)) - 1
+syn1 = 2*np.random.random((4,1)) - 1
+
+for j in np.arange(60000):
+
+	# Feed forward through layers 0, 1, and 2
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+    l2 = nonlin(np.dot(l1,syn1))
+
+    # how much did we miss the target value?
+    l2_error = y - l2
+    
+    if (j% 10000) == 0:
+        print("Error:" + str(np.mean(np.abs(l2_error))))
+        
+    # in what direction is the target value?
+    # were we really sure? if so, don't change too much.
+    l2_delta = l2_error*nonlin(l2,deriv=True)
+
+    # how much did each l1 value contribute to the l2 error (according to the weights)?
+    l1_error = l2_delta.dot(syn1.T)
+    
+    # in what direction is the target l1?
+    # were we really sure? if so, don't change too much.
+    l1_delta = l1_error * nonlin(l1,deriv=True)
+
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
+
+####USING SKLearn
+#https://stackabuse.com/introduction-to-neural-networks-with-scikit-learn/
+# Import required libraries
+
+# Location of dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# Assign colum names to the dataset
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+# Read dataset to pandas dataframe
+irisdata = pd.read_csv(url, names=names)
+irisdata.head() # dataset has five columns. The task is to predict the class (which are the values in the fifth column)
+# Assign data from first four columns to X variable
+X = irisdata.iloc[:, 0:4]
+
+# Assign data from first fifth columns to y variable
+y = irisdata.select_dtypes(include=[object])
+y.head()
+#You can see that the values in the y series are categorical.
+#However, neural networks work better with numerical data. 
+#Our next task is to convert these categorical values to numerical values.
+# But first let's see how many unique values we have in our y series. 
+y.Class.unique()
+
+#We have three unique classes 'Iris-setosa', 'Iris-versicolor' and 'Iris-virginica'. 
+#Let's convert these categorical values to numerical values. 
+#To do so we will use Scikit-Learn's LabelEncoder class.
+
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+
+y = y.apply(le.fit_transform)
+
+#To create training and test splits, execute the following script:
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+#Training and Predictions
+
+from sklearn.neural_network import MLPClassifier
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+mlp.fit(X_train, y_train.values.ravel())
+#PRediction
+
+predictions = mlp.predict(X_test)
+
+#Evaluating the Algorithm
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
